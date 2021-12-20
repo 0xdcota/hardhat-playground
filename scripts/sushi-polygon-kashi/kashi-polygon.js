8843b18907fb80fe475e614fc5c455a3e2e0c2c8
DCota <32775237+DaigaroCota@users.noreply.github.com>
2021-12-20 16:04:39 -0500

    query of kashi pairs

--- scripts/sushi-polygon-kashi/kashi-polygon.js
+++ scripts/sushi-polygon-kashi/kashi-polygon.js
@@ -7,8 +7,10 @@ const ERC20_ABI = require('./abi-erc20.json');
 
 const WBTC = '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6';
 const WETH = '0x7ceb23fd6bc0add59e62ac25578270cff1b9f619';
-const DAI = '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063';
-const MATIC = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270';
+const DAI = '0x8f3cf7ad23cd3cadbd9735aff958023239c6a063';
+const WMATIC = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270';
+const USDC = '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174';
+const USDT = '0xc2132D05D31c914a87C6611C10748AEb04B58e8F';
 
 const networkTest = async () => {
     let wallet = await hre.ethers.getSigners();
@@ -49,25 +51,37 @@ const extractGraphQLInfo = async () => {
 
 const main = async () => {
 
-    console.log("Filetering Data!");
+  console.log("Filetering Data!");
 
-    const kashiPairsPolygon = require('./output/kashi-polygon-symbols.json');
-    console.log(kashiPairsPolygon.length);
+  const kashiPairsPolygon = require('./output/kashi-polygon-symbols.json');
+  console.log(kashiPairsPolygon.length);
 
-    let pairsOfInterest = kashiPairsPolygon.filter( element => 
-        element.kashiPair.collateral.id === WETH
-    );
-    pairsOfInterest = pairsOfInterest.filter( element =>
-        element.kashiPair.asset.id === DAI    
-    );
+  const pairsOfInterest_ = kashiPairsPolygon.filter( element => 
+      element.kashiPair.collateral.id == WBTC.toLowerCase()
+  );
 
-    console.log(pairsOfInterest);
+  const pairsOfInterest = pairsOfInterest_.filter( element =>
+      element.kashiPair.asset.id == DAI.toLowerCase()    
+  );
+
+  console.log(pairsOfInterest);
 }
 
- main();
-//extractGraphQLInfo();
+main();
+// extractGraphQLInfo();
 
 const kmWETHDAI ="0x12d7906b1c9a2e0f73d251bafdbd369fed6f8c64";
+const kmWETHUSDC = '0xd51b929792cfcde30f2619e50e91513dcec89b23';
+const kmWETHUSDT = '0xef7f30c8f0763b83d8779fb90df99cb5e70425e8';
+
+const kmWBTCDAI = '0x99D6659207a7B0f4D906DFa3ff61f0F50E343555';
+const kmWBTCUSDC = '0x684575df2d01523fafcbf6768a4549d4309c7ea6';
+const kmWBTCUSDT = '0x0e55c1D982B65C489f699720133de8f83F4adC3b';
+
+
+const kmWMATICDAI = '0x13fdda9d4a0f009e6795ed3e7b8d0974f7baaa6b';
+const kmWMATICUSDC = '0xe4b3c431e29b15978556f55b2cd046be614f558d';
+
 
 
 
