865501d1287ea35e13f162cd57f34e25b62613bb
DCota <32775237+DaigaroCota@users.noreply.github.com>
2022-02-09 16:09:27 -0500

    proofreader contract and tests

---
+++ contracts/ProofReader.sol
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract ProofReader {
+  bytes32 public merkleRoot;
+
+  mapping(address => bool) public givePoints;
+
+  constructor(bytes32 _merkleroot) {
+    merkleRoot = _merkleroot;
+  }
+
+  function dummyEntryFunction(uint256 _aDummyNumber) external {
+    _aDummyNumber; // Function should do something here ...
+    _tryGetthePoints();
+  }
+
+  // Internal functions to extract proof
+
+  function _tryGetthePoints() internal {
+    (bool successGetProof, bytes32[] memory proof) = _getProofFromMessage();
+    require(successGetProof, "Cannot extract proof!");
+    require(_verify(msg.sender, proof), "cannot verify proof!");
+    givePoints[msg.sender] = true;
+  }
+
+  function _verify(address userAddress, bytes32[] memory proof) internal view returns (bool) {
+    bytes32 leaf = keccak256(abi.encodePacked(userAddress));
+    return MerkleProof.verify(proof, merkleRoot, leaf);
+  }
+
+  function _getProofFromMessage() internal pure returns (bool success, bytes32[] memory proof) {
+    uint8 dataSize;
+    uint8 bytes32slots;
+    uint8 proofcontent;
+    assembly {
+      dataSize := calldatasize()
+      bytes32slots := div(sub(dataSize, 36), 32)
+      proofcontent := sub(bytes32slots, 2)
+    }
+    require(dataSize > 100, "No proof detected!");
+
+    bytes memory data = msg.data[100:dataSize];
+    proof = _bytesToBytes32Array(data);
+
+    require(proof.length == proofcontent, "build proof failed!");
+
+    success = true;
+  }
+
+  function _bytesToBytes32Array(bytes memory data) private pure returns (bytes32[] memory) {
+    // Find 32 bytes segments nb
+    uint256 dataNb = data.length / 32;
+    // Create an array of dataNb elements
+    bytes32[] memory dataList = new bytes32[](dataNb);
+    // Start array index at 0
+    uint256 index = 0;
+    // Loop all 32 bytes segments
+    for (uint256 i = 32; i <= data.length; i = i + 32) {
+      bytes32 temp;
+      // Get 32 bytes from data
+      assembly {
+        temp := mload(add(data, i))
+      }
+      // Add extracted 32 bytes to list
+      dataList[index] = temp;
+      index++;
+    }
+    // Return data list
+    return (dataList);
+  }
+}
---
+++ test/proofreader.js
@@ -0,0 +1,107 @@
+const { ethers } = require("hardhat");
+const { expect } = require('chai');
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
+
+const DEBUG = false;
+
+const abiCoder = ethers.utils.defaultAbiCoder;
+
+const remove0xFromHexString = function (hexString) {
+  if (!(hexString === null || hexString === void 0 ? void 0 : hexString.toLowerCase().startsWith("0x"))) {
+    return hexString;
+  }
+  return hexString.substr(2);
+}
+
+const appendMerkleProof = function (unsignedTransaction, merkletree, unhashedleaf) {
+  let utx = unsignedTransaction;
+  let proof = merkletree.getHexProof(keccak256(unhashedleaf));
+  if (DEBUG) {
+    console.log("proof from js", proof);
+  }
+  proof = abiCoder.encode(['bytes32[]'], [proof]);
+  proof = remove0xFromHexString(proof);
+  utx.data = utx.data + proof;
+  return utx;
+}
+
+describe("Test Proof Extraction", function () {
+
+  let accounts;
+  let userEligible;
+  let userEligibleIndex;
+  let userNONeligible;
+  let userNONeligibleIndex;
+
+  let ProofReader;
+  let leaves;
+  let merkletree;
+  let merkleroot;
+  let proofreader;
+
+  before(async () => {
+    accounts = await ethers.getSigners();
+    userEligibleIndex = 0;
+    userEligible = accounts[userEligibleIndex];
+    userNONeligibleIndex = 19;
+    userNONeligible = accounts[userNONeligibleIndex];
+
+    ProofReader = await ethers.getContractFactory('ProofReader');
+    leaves = require('./sample-merkle-leaves.json');
+    merkletree = new MerkleTree(leaves, keccak256, { hashLeaves: true, sortPairs: true });
+    merkleroot = merkletree.getHexRoot();
+    proofreader = await ProofReader.deploy(merkleroot);
+  });
+
+  it('Should revert if no passed proof', async () => {
+    await expect(proofreader.connect(userEligible).dummyEntryFunction(100)).to.be.revertedWith("No proof detected!");
+  });
+
+  it('Should revert if eligible user passes wrong proof', async () => {
+    let utx = await proofreader.connect(userEligible).populateTransaction.dummyEntryFunction(100);
+    utx = appendMerkleProof(utx, merkletree, leaves[userEligibleIndex]);
+
+    // Modify proof to be wrong.
+    function setCharAt(str, index, chr) {
+      if (index > str.length - 1) return str;
+      return str.substring(0, index) + chr + str.substring(index + 1);
+    }
+    const dataLength = (utx.data).length;
+    utx.data = setCharAt(utx.data, dataLength - 10, 'f');
+
+    if (DEBUG) {
+      console.log("modified-utx-proof", utx);
+    }
+
+    await expect(userEligible.sendTransaction(utx)).to.be.revertedWith("cannot verify proof!");
+  });
+
+  it('Should revert if NON-eligible user passes a pretended proof', async () => {
+    let utx = await proofreader.connect(userNONeligible).populateTransaction.dummyEntryFunction(100);
+
+    // Passes the proof of an eligible user
+    utx = appendMerkleProof(utx, merkletree, leaves[userEligibleIndex]);
+
+    if (DEBUG) {
+      console.log("utx-proof", utx);
+    }
+
+    await expect(userNONeligible.sendTransaction(utx)).to.be.revertedWith("cannot verify proof!");
+  });
+
+  it("Should return true for 'givePoints' mapping, if eligible user passes correct proof", async () => {
+    let utx = await proofreader.connect(userEligible).populateTransaction.dummyEntryFunction(100);
+    utx = appendMerkleProof(utx, merkletree, leaves[0]);
+
+    if (DEBUG) {
+      console.log("utx+proof", utx);
+    }
+    await userEligible.sendTransaction(utx);
+
+    const check = await proofreader.givePoints(userEligible.address);
+    expect(check).to.eq(true);
+  });
+
+});
+
